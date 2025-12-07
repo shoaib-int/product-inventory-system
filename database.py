645b1ba1f99f730e2d7dd24ad794fbc0a93eb1b5
shoaib-int <146425023+shoaib-int@users.noreply.github.com>
2025-12-07 23:40:27 +0530

    Adding project files

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+db_url="postgresql://postgres:Shoaib9450*@localhost:5432/telusko"
+engine=create_engine(db_url)
+
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
