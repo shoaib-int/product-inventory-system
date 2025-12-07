0b4dcc42c946172b8633fb237a9da967c4dbbf6c
shoaib-int <146425023+shoaib-int@users.noreply.github.com>
2025-12-08 00:19:28 +0530

    fix bug

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-db_url="postgresql://postgres:Shoaib9450*@localhost:5432/telusko"
+db_url="postgresql://postgres:Shoaib9450%2A@localhost:5432/telusko"
 engine=create_engine(db_url)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
