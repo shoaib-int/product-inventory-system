3107445c93f8aa7a85455f6388febcbedc778375
shoaib-int <146425023+shoaib-int@users.noreply.github.com>
2026-01-16 22:29:40 +0530

    Add the db  url

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url="postgresql://enventory_user:8OVKqsJiY6aAcrfzocNrnLmJ61JF6t34@dpg-d4qt55s9c44c73bjinjg-a.singapore-postgres.render.com/enventory"
+db_url="postgresql://enventory_bm3i_user:JLtV31oFHgQBOrSdnROzb7u8opOzbzaG@dpg-d5l6lce3jp1c739422tg-a.singapore-postgres.render.com/enventory_bm3i"
 engine=create_engine(db_url)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
